34890415f34e4ec28028235f5fc6a29699098fcc
Asus <Asus@Irene>
2026-01-28 22:27:47 +0100

    Versión inicial del proyecto

---
+++ src/main/webapp/sw.js
@@ -0,0 +1,47 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+const CACHE_NAME = "gestor-clases-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./menu.jsp",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// 1. INSTALACIÓN: Guardamos archivos básicos en caché
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 2. PETICIONES: Intentamos ir a internet, si falla, tiramos de caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// 3. ACTIVACIÓN: Limpiamos cachés viejas si actualizas la app
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
